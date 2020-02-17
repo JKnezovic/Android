eae3c6b8c66bb194fac297762f846921c1cb3bb0
katarinacudina <ka.cudina@gmail.com>
2020-02-18 00:26:08 +0100

    medicine list, working searchbar, medicine details style

--- app/src/main/java/com/example/terapija/LijekoviFragment.java
+++ app/src/main/java/com/example/terapija/LijekoviFragment.java
@@ -1,10 +1,17 @@
 package com.example.terapija;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,7 +24,11 @@ import java.util.List;
 
 public class LijekoviFragment extends Fragment {
 
+    private SearchView.OnQueryTextListener queryTextListener;
+    private SearchView searchView = null;
+
     RecyclerView recyclerView;
+    MyListAdapter listAdapter;
 
     @Nullable
     @Override
@@ -27,13 +38,16 @@ public class LijekoviFragment extends Fragment {
         recyclerView = (RecyclerView) view.findViewById(R.id.listRecyclerView);
         recyclerView.setHasFixedSize(true);
 
+        hasOptionsMenu();
+
         //layout manager
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
+
         new FirebaseDatabaseHelper("drugs").readMedicine(new FirebaseDatabaseHelper.DataStatus() {
             @Override
             public void DataIsLoaded(List<Medicine> medicines, List<String> keys) {
-                MyListAdapter listAdapter = new MyListAdapter(getActivity(), medicines);
+                listAdapter = new MyListAdapter(getActivity(), medicines);
                 recyclerView.setAdapter(listAdapter);
             }
 
@@ -60,6 +74,73 @@ public class LijekoviFragment extends Fragment {
         return view;
         }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+   /* @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+
+        inflater.inflate(R.menu.example_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+
+            queryTextListener = new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    Log.i("onQueryTextChange", newText);
+
+                    return true;
+                }
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    Log.i("onQueryTextSubmit", query);
+
+                    return true;
+                }
+            };
+            searchView.setOnQueryTextListener(queryTextListener);
+        }
+        super.onCreateOptionsMenu(menu, inflater);
+    }*/
+
+
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    inflater.inflate(R.menu.example_menu, menu);
+
+    MenuItem searchItem = menu.findItem(R.id.search);
+
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                listAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
+
 
     }
+}
 
